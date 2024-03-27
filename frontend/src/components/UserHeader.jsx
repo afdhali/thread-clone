933c08bbbc50991defffea3f563d0a4cdd8aa085
afdhali <afdhali.apreto@gmail.com>
2024-03-27 19:11:11 +0700

    adding custom css for user header component

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -1,11 +1,14 @@
-import { Box, VStack, Flex, Avatar, Text } from "@chakra-ui/react";
+import { Box, VStack, Flex, Avatar, Text, Link } from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
 
 export default function UserHeader() {
   return (
     <VStack gap={4} alignItems={"start"}>
       <Flex justifyContent={"space-between"} w={"full"}>
         <Box>
-          <Text fontSize={"2xl"}>Mark Zuckerberg</Text>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Mark Zuckerberg
+          </Text>
           <Flex gap={2} alignItems={"center"}>
             <Text fontSize={"sm"}>markzuckerberg</Text>
             <Text fontSize={"x-small"} bg={"gray.dark"} color={"gray.light"}>
@@ -16,6 +19,19 @@ export default function UserHeader() {
         <Box>
           <Avatar name="Mark Zuckerberg" src="/zuck-avatar.png" size={"xl"} />
         </Box>
+      </Flex>
+      <Text>This is a Biography</Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>3.2k Followers</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+        </Flex>
       </Flex>
     </VStack>
   );
