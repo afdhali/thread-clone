afc305ed71012ec60cae41137386bfde45e80608
afdhali <afdhali.apreto@gmail.com>
2024-03-27 21:09:24 +0700

    add Actions component into User Post

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 
 export function UserPage() {
   return (
     <>
       <UserHeader />
+      <UserPost />
     </>
   );
 }
