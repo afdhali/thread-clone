2b097171043478fde0507f056cc8e4f02dccda7d
afdhali <afdhali.apreto@gmail.com>
2024-04-03 16:35:04 +0700

    completed signup and login API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,4 +47,37 @@ const signupUser = async (req, res) => {
   }
 };
 
-export { signupUser };
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    ); // give check empty string to avoid error in compare if not have username
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid  Credentials Login" });
+
+    if (user.isFrozen) {
+      user.isFrozen = false;
+      await user.save();
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in loginUser: ", error.message);
+  }
+};
+
+export { signupUser, loginUser };
