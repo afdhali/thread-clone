ac914404b6213ee7e5d4c09cc1440e5331ca9f18
afdhali <afdhali.apreto@gmail.com>
2024-04-04 15:48:08 +0700

    create get and delete post API

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -36,4 +36,37 @@ const createPost = async (req, res) => {
   }
 };
 
-export { createPost };
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not Found" });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in getPost by id: ", error.message);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not Found" });
+    }
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ error: "Unauthorized to delete post" });
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Post Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error on delete post: ", error.message);
+  }
+};
+
+export { createPost, getPost, deletePost };
