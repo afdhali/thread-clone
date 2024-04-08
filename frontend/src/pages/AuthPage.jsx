38b0356b4a1cc533f70dac4b10a55247cd2954f2
afdhali <afdhali.apreto@gmail.com>
2024-04-08 14:37:47 +0700

    create Login Register UI

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,10 @@
+import SignupCard from "../components/SignupCard";
+import LoginCard from "../components/Login";
+
+export default function AuthPage() {
+  return (
+    <>
+      <LoginCard />
+    </>
+  );
+}
