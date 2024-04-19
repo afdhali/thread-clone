1d8559ce0027ab172d4087fa97258b082b305e19
afdhali <afdhali.apreto@gmail.com>
2024-04-19 13:41:51 +0700

    testing getMessage API

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { sendMessage } from "../controllers/messageController.js";
+import { sendMessage, getMessages } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, sendMessage);
+router.get("/:otherUserId", protectRoute, getMessages);
 
 export default router;
